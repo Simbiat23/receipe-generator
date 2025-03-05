15b8c593ead740756caa2847ea654dce69f89e29
Simbiat23 <155582634+Simbiat23@users.noreply.github.com>
2025-03-05 17:54:07 +0000

    Added the API key

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-function generateReceipe(event) {
-  event.preventDefault();
-
+function displayRecipe(response) {
   new Typewriter("#receipe", {
-    strings: ` 1. Preheat your oven to 350°F (175°C). Grease and flour two 9-inch
-          round cake pans.
-         
-          2. In a large bowl, whisk together flour, sugar, baking soda, salt,
-          and cocoa powder.
-          
-          3. In another bowl, combine oil, buttermilk, eggs, food coloring`,
+    strings: response.data.answer,
     autoStart: true,
     cursor: "",
     delay: 1,
   });
 }
-let receipeFormElememt = document.querySelector("#receipe-generator-form");
-receipeFormElememt.addEventListener("submit", generateReceipe);
+
+function generateRecipe(event) {
+  event.preventDefault();
+  let intructionInput = document.querySelector("#user-recipe-input");
+
+  let apiKey = "t07aaefccae3394of62526e7dc0c0bad";
+  let prompt = `Generate a recipe based on: ${intructionInput.value}. Provide the recipe name, a list of ingredients with quantities, and step-by-step instructions. Please separat the recipe(using a line brake) in order of: recipe name, a list of ingredients with quantities and step-by-step instructions,also display the recipe in basic HTML without saying it is in HTML format `;
+  let context =
+    "You are a professional chef generating delicious recipes, make sure use the users input";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayRecipe);
+}
+
+let recipeFormElememt = document.querySelector("#recipe-generator-form");
+recipeFormElememt.addEventListener("submit", generateRecipe);
